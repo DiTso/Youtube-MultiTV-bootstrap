8b097172cc064b0b914d28c7b91f48d391964854
Nicola1971 <modxit@modx.it>
2015-03-28 17:57:12 +0100

    1.1 RC

--- assets/tvs/multitv/configs/youtube-fr.config.inc.php
+++ assets/tvs/multitv/configs/youtube-fr.config.inc.php
@@ -16,6 +16,12 @@ $settings['fields'] = array(
           'width' => '180',
         'elements' => 'video==0||thumb==1'
     ),
+    'thumbzise' => array(
+        'caption' => 'Taille des Miniature',
+        'type' => 'dropdown',
+          'width' => '180',
+        'elements' => 'maxres==maxresdefault||default==0',
+    ),
     'ratio' => array(
         'caption' => 'Format (Vidéo)',
         'type' => 'dropdown',
@@ -26,7 +32,7 @@ $settings['fields'] = array(
 $settings['templates'] = array(
 	'outerTpl' => '[+wrapper+]',
 	'rowTpl' => '[+title:ne=``:then=`<h3>[+title+]</h3>`:else=`<p></p>`+]
-    [+showthumb:is=`1`:then=`<a class="zoomthumbnail" rel="gallerysw" title="[+title+]" href="https://www.youtube.com/watch?v=[+video+]"><img class="img-responsive img-thumbnail" src="https://img.youtube.com/vi/[+video+]/maxresdefault.jpg" alt="[+title+]" title="[+title+]" /></a>`:else=`<div class="embed-responsive embed-responsive-[+ratio+]"> <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/[+video+]"></iframe>
+    [+showthumb:is=`1`:then=`<a class="zoomthumbnail" rel="gallerysw" title="[+title+]" href="https://www.youtube.com/watch?v=[+video+]"><div class="img-overlay"><img class="img-responsive img-thumbnail" src="https://img.youtube.com/vi/[+video+]/[+thumbzise+].jpg" alt="[+title+]" title="[+title+]" /><div class="fa-overlay"><span> <i class="fa fa-2x fa-youtube-play"></i></span> </div></div></a>`:else=`<div class="embed-responsive embed-responsive-[+ratio+]"> <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/[+video+]"></iframe>
 </div>`+]'
 		)
 ?>
---
+++ install/assets/chunks/youtubetv-css.tpl
@@ -0,0 +1,15 @@
+/**
+ * youtubetv-css
+ *
+ * include css for youtubetv
+ *
+ * @author      tattoocms.it
+ * @category	chunk
+ * @version 	1.0
+ * @license 	http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
+ * @internal	@modx_category MultiTV add-ons
+ * @internal    @installset base
+ * @internal    @overwrite false
+ */
+
+<link rel="stylesheet" type="text/css" href="assets/templates/common/css/youtubetv/youtubetv.css" media="screen" />
